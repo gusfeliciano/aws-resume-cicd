39f1a70e8c826ae3c1b94b0a80519d74eb802e64
gusfeliciano <gustav213@gmail.com>
2024-07-10 11:53:59 -0600

    Initial commit

---
+++ test/visitor-counter-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VisitorCounterCdk from '../lib/visitor-counter-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/visitor-counter-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VisitorCounterCdk.VisitorCounterCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
